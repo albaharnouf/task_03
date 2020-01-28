2f55a88abba51dab326a1a562b3a4cc22cfedcda
Nouf Albahar <albahar.nouf@gmail.com>
2020-01-28 11:10:44 +0300

    restaurantz

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,16 +4,32 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
-    context = {
-
+    context ={
+        "my_list": [
+            {
+            "restaurant_name": "Little Rubys",
+            "food_type": "Yummy"
+        },
+        {
+            "restaurant_name": "Tatami",
+            "food_type": "Japanese"
+        },
+        {
+            "restaurant_name": "Burger King",
+            "food_type": "Fast food"
+        },
+        ]
     }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
-
-    context = {
+    context ={
+        "my_object":
+        {
+            "restaurant_name": "Little Rubys",
+            "food_type": "Yummy"
+        }
 
     }
     return render(request, 'detail.html', context)
